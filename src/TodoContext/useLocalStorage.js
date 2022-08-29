fc25237a9a145c4db78220b2c45a6e963aab37d9
Julio Reyes <jreysgo@gmail.com>
2022-08-29 19:33:42 -0400

    Agregar íconos, loading, y EmptyTodos

--- src/TodoContext/useLocalStorage.js
+++ src/TodoContext/useLocalStorage.js
@@ -1,48 +1,47 @@
-import React from "react";
+import React from 'react';
 
-
-function useLocalStorage(itemName, initialValue) { // custom hook para manejar el localStorage
-    const [error, setError] = React.useState(false);
-    const [loading, setLoading] = React.useState(true);
-    const [item, setItem] = React.useState(initialValue);
-    
-    React.useEffect(() => { // useEffect para manejar el localStorage y el estado
-      setTimeout(() => { // setTimeout para simular el tiempo de carga de la API
-        try {
-          const localStorageItem = localStorage.getItem(itemName);
-          let parsedItem;
-          
-          if (!localStorageItem) { // si no hay item en el localStorage se guarda el initialValue
-            localStorage.setItem(itemName, JSON.stringify(initialValue)); // se guarda el initialValue en el localStorage
-            parsedItem = initialValue;
-          } else {
-            parsedItem = JSON.parse(localStorageItem); // se parsea el item del localStorage para convertirlo en un objeto
-          }
+function useLocalStorage(itemName, initialValue) {
+  const [error, setError] = React.useState(false);
+  const [loading, setLoading] = React.useState(true);
+  const [item, setItem] = React.useState(initialValue);
   
-          setItem(parsedItem); // se guarda el item en el estado
-          setLoading(false);
-        } catch(error) {
-          setError(error);
-        }
-      }, 1000);
-    });
-    
-    const saveItem = (newItem) => {
+  React.useEffect(() => {
+    setTimeout(() => {
       try {
-        const stringifiedItem = JSON.stringify(newItem);
-        localStorage.setItem(itemName, stringifiedItem);
-        setItem(newItem);
+        const localStorageItem = localStorage.getItem(itemName);
+        let parsedItem;
+        
+        if (!localStorageItem) {
+          localStorage.setItem(itemName, JSON.stringify(initialValue));
+          parsedItem = initialValue;
+        } else {
+          parsedItem = JSON.parse(localStorageItem);
+        }
+
+        setItem(parsedItem);
+        setLoading(false);
       } catch(error) {
         setError(error);
       }
-    };
+    }, 3000);
+  });
   
-    return {
-      item,
-      saveItem,
-      loading,
-      error,
-    };
-  }
+  const saveItem = (newItem) => {
+    try {
+      const stringifiedItem = JSON.stringify(newItem);
+      localStorage.setItem(itemName, stringifiedItem);
+      setItem(newItem);
+    } catch(error) {
+      setError(error);
+    }
+  };
+
+  return {
+    item,
+    saveItem,
+    loading,
+    error,
+  };
+}
 
 export { useLocalStorage };
--- src/TodoList/index.js
+++ src/TodoList/index.js
@@ -1,14 +1,14 @@
-import React from "react";
-import './TodoList.css';
+import React from 'react';
+import './TodoList.css'
 
-function TodoList(props){
-    return(
-        <section>
-            <ul>
-                {props.children}
-            </ul>
-        </section>
-    );
+function TodoList(props) {
+  return (
+    <section>
+      <ul>
+        {props.children}
+      </ul>
+    </section>
+  );
 }
 
-export { TodoList};
+export { TodoList };
